b7d3dbe0f4e9e5702fd08a4952f4030c36722e1d
Matthew Bertello <matthew.bertello@gmail.com>
2023-10-14 13:43:43 -0500

    Create project

---
+++ PsychologyPracticeProblemApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PsychologyPracticeProblemApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
