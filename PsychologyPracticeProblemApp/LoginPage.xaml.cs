fa6d04171aee3446e158757d3c631db40401cb25
Matthew Bertello <matthew.bertello@gmail.com>
2023-10-16 17:59:48 -0500

    Creating login page

---
+++ PsychologyPracticeProblemApp/LoginPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychologyPracticeProblemApp
+{
+    public partial class LoginPage : ContentPage
+    {
+        int count = 0;
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
