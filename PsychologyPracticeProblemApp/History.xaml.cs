1f0d49df61dbf568b2360f4245433b0a9c4fe734
CloseRange <michael.m.hulbert@gmail.com>
2023-10-18 12:54:58 -0500

    Made Lists Work
    HistoryLog works now

--- PsychologyPracticeProblemApp/History.xaml.cs
+++ PsychologyPracticeProblemApp/History.xaml.cs
@@ -1,9 +1,17 @@
+using System.Collections.ObjectModel;
+
 namespace PsychologyPracticeProblemApp;
 
-public partial class History : ContentPage
-{
-	public History()
+public partial class History : ContentPage {
+	//private ObservableCollection<HistoryLog> history = new ObservableCollection<HistoryLog>();
+    public ObservableCollection<HistoryLog> Hist { get; set; }
+    public History()
 	{
 		InitializeComponent();
-	}
+        Hist = new ObservableCollection<HistoryLog> {
+            new HistoryLog() { Type = "Dependent Test", Date = DateTime.Now, Answer = 10525 },
+            new HistoryLog() { Type = "Dependent Test", Date = DateTime.Now, Answer = 10525 }
+        };
+        BindingContext = this;
+    }
 }
