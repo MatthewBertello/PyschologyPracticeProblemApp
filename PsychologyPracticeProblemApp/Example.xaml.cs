3cd5e54dc7627c323c448376e73f434bfd1d9285
Matthew Bertello <matthew.bertello@gmail.com>
2023-10-18 13:11:54 -0500

    Add the table to the example page

--- PsychologyPracticeProblemApp/Example.xaml.cs
+++ PsychologyPracticeProblemApp/Example.xaml.cs
@@ -1,9 +1,35 @@
+using System.Collections.ObjectModel;
+
 namespace PsychologyPracticeProblemApp;
 
 public partial class Example : ContentPage
 {
-	public Example()
+    public class DataRow
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public String XString { get { return X.ToString(); } }
+        public String YString { get { return Y.ToString(); } }
+        public String ZString { get { return Z.ToString(); } }
+
+        public DataRow(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+
+    ObservableCollection<DataRow> dataRows = new ObservableCollection<DataRow>();
+    public Example()
 	{
-		InitializeComponent();
-	}
+        for (int i = 0; i < 15; i++)
+        {
+            dataRows.Add(new DataRow(i, i * 2, i * 3));
+        }
+        InitializeComponent();
+        BindingContext = dataRows;
+    }
 }
