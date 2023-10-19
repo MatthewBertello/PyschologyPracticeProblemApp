a050ecea4a591359edbe5a97e738474a812d6a63
mise11 <115748357+mise11@users.noreply.github.com>
2023-10-18 19:26:40 -0500

    Add files via upload

--- PsychologyPracticeProblemApp/History.xaml.cs
+++ PsychologyPracticeProblemApp/History.xaml.cs
@@ -2,16 +2,34 @@ using System.Collections.ObjectModel;
 
 namespace PsychologyPracticeProblemApp;
 
-public partial class History : ContentPage {
-	//private ObservableCollection<HistoryLog> history = new ObservableCollection<HistoryLog>();
-    public ObservableCollection<HistoryLog> Hist { get; set; }
+public partial class History : ContentPage
+{
+    public class Data
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public String XString { get { return X.ToString(); } }
+        public String YString { get { return Y.ToString(); } }
+        public String ZString { get { return Z.ToString(); } }
+
+        public Data(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+
+    ObservableCollection<Data> dataR = new ObservableCollection<Data>();
     public History()
-	{
-		InitializeComponent();
-        Hist = new ObservableCollection<HistoryLog> {
-            new HistoryLog() { Type = "Dependent Test", Date = DateTime.Now, Answer = 10525 },
-            new HistoryLog() { Type = "Dependent Test", Date = DateTime.Now, Answer = 10525 }
-        };
-        BindingContext = this;
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            dataR.Add(new Data(i, i * 2, i * 3));
+        }
+        InitializeComponent();
+        BindingContext = dataR;
     }
 }
