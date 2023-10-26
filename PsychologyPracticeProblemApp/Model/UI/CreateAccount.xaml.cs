c71286efb91f3546230a55ff9507c3a508816b5e
CloseRange <michael.m.hulbert@gmail.com>
2023-10-26 18:50:29 -0500

    Refactoring File Structure
    Moved files around to make it a little more clear where things are

---
+++ PsychologyPracticeProblemApp/Model/UI/CreateAccount.xaml.cs
@@ -0,0 +1,14 @@
+/*
+ * Michael Meisenburg
+ * Date: 10/18/2023
+*/
+
+namespace PsychologyPracticeProblemApp;
+
+public partial class CreateAccount : ContentPage
+{
+	public CreateAccount()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PsychologyPracticeProblemApp/Model/UI/History.xaml.cs
@@ -0,0 +1,40 @@
+/*
+ * Michael Meisenburg
+ * Date: 10/18/2023
+*/
+
+using System.Collections.ObjectModel;
+
+namespace PsychologyPracticeProblemApp;
+
+public partial class History : ContentPage
+{
+    public class Data
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public String XString { get { return X.ToString(); } }
+        public String YString { get { return Y.ToString(); } }
+        public String ZString { get { return Z.ToString(); } }
+
+        public Data(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+
+    ObservableCollection<Data> dataR = new ObservableCollection<Data>();
+    public History()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            dataR.Add(new Data(i, i * 2, i * 3));
+        }
+        InitializeComponent();
+        BindingContext = dataR;
+    }
+}
