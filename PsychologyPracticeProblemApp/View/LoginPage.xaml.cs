db5cb966b372a4837aa75e7d45e72f150b36c027
mise11 <115748357+mise11@users.noreply.github.com>
2023-11-14 22:02:39 -0600

    Update LoginPage.xaml.cs

--- PsychologyPracticeProblemApp/View/LoginPage.xaml.cs
+++ PsychologyPracticeProblemApp/View/LoginPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Matthew Bertello
  * Date: 10/18/2023
 */
@@ -24,34 +24,35 @@ namespace PsychologyPracticeProblemApp
             User.LoginGuest();
             await Navigation.PushAsync(new HomePage());
 
-        }  
+        }
+
+
+        public async void OnLogin(object sender, EventArgs e)
+        {
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+
+            // Check if username and password are entered
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                await DisplayAlert("Error", "Please enter both username and password.", "OK");
+                return;
+            }
+
+            // Retrieve user data from the database
+            User user = Database.GetUser(username, password);
+
+            if (user != null)
+            {
+                // Successful login
+                await DisplayAlert("Login Successful", "Welcome, " + user.FirstName + "!", "OK");
+                await Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                // Failed login
+                await DisplayAlert("Login Failed", "Invalid username or password. Please try again.", "OK");
+            }
+        }
     }
-
-     public async void OnLogin(object sender, EventArgs e)
-     {
-     string username = UsernameEntry.Text;
-     string password = PasswordEntry.Text;
-
-     // Check if username and password are entered
-     if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
-     {
-         await DisplayAlert("Error", "Please enter both username and password.", "OK");
-         return;
-     }
-
-     // Retrieve user data from the database
-     User user = Database.GetUser(username, password);
-
-     if (user != null)
-     {
-         // Successful login
-         await DisplayAlert("Login Successful", "Welcome, " + user.FirstName + "!", "OK");
-         await Navigation.PushAsync(new HomePage());
-     }
-     else
-     {
-         // Failed login
-         await DisplayAlert("Login Failed", "Invalid username or password. Please try again.", "OK");
-     }
- }
 }
