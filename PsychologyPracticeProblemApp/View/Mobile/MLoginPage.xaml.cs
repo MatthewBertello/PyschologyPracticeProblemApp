ecce3dd65f50fa78e84d811a070d00ea99d2411e
CloseRange <michael.m.hulbert@gmail.com>
2023-11-16 19:00:25 -0600

    Fixed some basic styling

--- PsychologyPracticeProblemApp/View/Mobile/MLoginPage.xaml.cs
+++ PsychologyPracticeProblemApp/View/Mobile/MLoginPage.xaml.cs
@@ -5,24 +5,42 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PsychologyPracticeProblemApp
-{
-    public partial class MLoginPage : ContentPage
+namespace PsychologyPracticeProblemApp;
+
+public partial class MLoginPage : ContentPage, INotifyPropertyChanged {
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public string ErrorMessage { get; set; } = string.Empty;
+    public MLoginPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+    public async void OnGuest(object sender, EventArgs e)
+    {
+        User.LoginGuest();
+        await Navigation.PushAsync(new HomePage());
+    }
+    public async void OnLogin(object sender, EventArgs e)
+    {
+        ErrorMessage = "";
+        string username = PasswordEntry.Text;
+        string password = PasswordEntry.Text;
+        User user = User.Login(username, password);
+        //if(user != null) await Navigation.PushAsync(new HomePage());
+        //else ErrorMessage = "Invalid Username or Password!";
+
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ErrorMessage"));
+    }
+
+    private void OnCreateAccount(object sender, EventArgs e)
     {
 
-        public MLoginPage()
-        {
-            InitializeComponent();
-        }
-        public async void OnGuest(object sender, EventArgs e)
-        {
-            User.LoginGuest();
-            await Navigation.PushAsync(new HomePage());
-
-        }
     }
 }
