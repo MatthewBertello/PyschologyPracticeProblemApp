9c915e90b05b3212ea7d3b355ebb7d96b45cb79f
CloseRange <michael.m.hulbert@gmail.com>
2023-11-16 18:38:29 -0600

    Fixed Conflicts
    Fixed conflicts with no login cs page

---
+++ PsychologyPracticeProblemApp/View/Mobile/MLoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Matthew Bertello
+ * Date: 10/18/2023
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychologyPracticeProblemApp
+{
+    public partial class MLoginPage : ContentPage
+    {
+
+        public MLoginPage()
+        {
+            InitializeComponent();
+        }
+        public async void OnGuest(object sender, EventArgs e)
+        {
+            User.LoginGuest();
+            await Navigation.PushAsync(new HomePage());
+
+        }
+    }
+}
