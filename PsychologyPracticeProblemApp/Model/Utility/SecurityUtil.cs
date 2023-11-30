83366ad39c2d64cc9f8cb2e12ba92c27c47ef6e5
mise11 <115748357+mise11@users.noreply.github.com>
2023-11-29 19:11:13 -0600

    Update SecurityUtil.cs

--- PsychologyPracticeProblemApp/Model/Utility/SecurityUtil.cs
+++ PsychologyPracticeProblemApp/Model/Utility/SecurityUtil.cs
@@ -6,47 +6,45 @@ using System.Threading.Tasks;
 using System.Security.Cryptography;
 
 namespace PsychologyPracticeProblemApp.Model.Utility;
+
 public static class SecurityUtil {
    
-    /// <summary>
-    /// Encrypts the message
-    /// </summary>
-    /// <param name="message"></param>
-    /// <returns></returns>
-    public static string Encrypt(string message)
-    {
-        //salt = new Guid().ToString(); // simple random salt
-        // message to bytes
-        byte[] data = Encoding.ASCII.GetBytes(message);
-        // hash the bytes
-        SHA384 sha = SHA384.Create();
-        byte[] result = sha.ComputeHash(data);
-        // bytes back to string
-        return Encoding.UTF8.GetString(result);
-    }
+   /// <summary>
+   /// Encrypts the message
+   /// </summary>
+   /// <param name="message"></param>
+   /// <returns></returns>
+   public static string Encrypt(string message)
+   {
+       //salt = new Guid().ToString(); // simple random salt
+       // message to bytes
+       byte[] data = Encoding.ASCII.GetBytes(message);
+       // hash the bytes
+       SHA384 sha = SHA384.Create();
+       byte[] result = sha.ComputeHash(data);
+       // bytes back to string
+       return Encoding.UTF8.GetString(result);
+   }
     
     /// <summary>
-    /// Hash the password using BCrypt
+    /// gets the Encrypted password
     /// </summary>
     /// <param name="password"></param>
     /// <returns></returns>
     public static string HashPassword(string password)
     {
         return Encrypt(password);
-        //return BCrypt.Net.BCrypt.HashPassword(password);
     }
 
     /// <summary>
-    /// Verify if the entered password matches the hashed password using BCrypt
+    /// Verifys the Encrypted password
     /// </summary>
     /// <param name="enteredPassword"></param>
     /// <param name="hashedPassword"></param>
-    ///<param name="salt"></param>
     /// <returns></returns>
     public static bool VerifyPassword(string enteredPassword, string hashedPassword)
     {
         return Encrypt(enteredPassword) == hashedPassword;
-        //return BCrypt.Net.BCrypt.Verify(enteredPassword, hashedPassword);
     }
 
 }
